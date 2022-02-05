e1bd07e528200d64f27feebb28caa1d657ab3bde
gavin <gavin.lim@alumni.ubc.ca>
2022-02-05 15:54:07 -0800

    Add new project button

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,19 +1,34 @@
+// Sidebar header
+function addHeader() {
+    const sidebarHeader = document.createElement('div');
+    sidebarHeader.setAttribute('id', 'sidebar_header');
+    sidebarHeader.innerHTML = 'Projects';
+    return sidebarHeader;
+}
 
-// const newProjBtn = document.createElement('div');
-// newProjBtn.classList.add('new-proj-btn');
-// newProjBtn.innerHTML = 'New Project';
-// sidebar.appendChild(newProjBtn);
+// New Project button
+function addNewProjBtn() {
+    const newProjBtn = document.createElement('div');
+    newProjBtn.classList.add('new-proj-btn', 'sidebar-btn');
+
+    const addIcon = document.createElement('i');
+    addIcon.classList.add('fas', 'fa-plus', 'sidebar-icon');
+    newProjBtn.appendChild(addIcon);
+
+    const content = document.createElement('div');
+    content.textContent = 'New Project';
+    newProjBtn.appendChild(content);
+
+    return newProjBtn;
+}
 
 function initSidebar() {
 
     const sidebar = document.createElement('div');
     sidebar.setAttribute('id', 'sidebar');
-
-
-    const sidebar_header = document.createElement('div');
-    sidebar_header.setAttribute('id', 'sidebar_header');
-    sidebar_header.innerHTML = 'Projects';
-    sidebar.appendChild(sidebar_header);
+    
+    sidebar.appendChild(addHeader());
+    sidebar.appendChild(addNewProjBtn());
 
     return sidebar;
 }
