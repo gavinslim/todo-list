24292bfe1165568ed20cbe17888fabcb687681f1
gavin <gavin.lim@alumni.ubc.ca>
2022-02-10 22:12:58 -0800

    Add new branch

--- src/taskpage.js
+++ src/taskpage.js
@@ -1,10 +1,37 @@
 
-
 function initTaskpage() {
     const taskPage = document.createElement('div');
     taskPage.setAttribute('id', 'task-page');
 
+    taskPage.appendChild(loadTaskHeader())
+    taskPage.appendChild(loadTaskList());
     return taskPage;
+}
+
+function loadTaskHeader() {
+    const taskHeader = document.createElement('div');
+    taskHeader.setAttribute('id', 'task-header');
+    taskHeader.textContent = 'Tasks';
+    // var projectList = document.querySelector('.project-list');
+
+    // projectList.forEach(project => {
+    //     console.log(project);
+    // });
+
+    return taskHeader;
+}
+
+function loadTaskList() {
+    const taskList = document.createElement('div');
+    taskList.classList.add('task-list');
+
+    const projects = document.getElementsByClassName('project-list');
+    console.log(projects);
+    return taskList;
+}
+
+function populateTaskList(tasks = [], taskList) {
+
 }
 
 export default initTaskpage;
