99028fe6131685987308cb65812f7fc7b431e02b
gavin <gavin.lim@alumni.ubc.ca>
2022-02-19 01:14:19 -0800

    Fix styling

--- src/taskpage.js
+++ src/taskpage.js
@@ -14,6 +14,7 @@ export function initTaskpage() {
 function loadTaskHeader() {
     const taskHeader = document.createElement('div');
     taskHeader.setAttribute('id', 'task-header');
+    taskHeader.classList.add('disable-select');
     taskHeader.innerHTML = 'Tasks';
     return taskHeader;
 }
